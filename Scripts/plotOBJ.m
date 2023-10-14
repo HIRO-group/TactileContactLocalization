72b3c03043fe41dee77dda54bbdeadd701b93247
Your Name Carson <cako7425@colorado.edu>
2023-10-14 00:26:44 -0600

    Data Collection Visualizer Working

--- Scripts/plotOBJ.m
+++ Scripts/plotOBJ.m
@@ -1,5 +1,6 @@
 % Updates the specified axis to show an obj file
 
-function plotObj(UIaxis, vertices, facets)
-
+function plotOBJ(UIAxes, vertices, facets)
+    %scatter3(UIAxes, vertices(:,1), vertices(:,2), vertices(:,3),10, 'k', 'filled');
+    patch(UIAxes, 'Faces',facets,'Vertices',vertices,'FaceColor','green', 'EdgeColor', 'k', 'Linestyle', ':');
 end
