534a1b14006f38f2a35653ae8e10c535b99ae892
Your Name Carson <cako7425@colorado.edu>
2023-11-15 16:01:39 -0700

    No vertices model added

--- Scripts/Locate_Sensors_Scripts/test_NLS.m
+++ Scripts/Locate_Sensors_Scripts/test_NLS.m
@@ -34,8 +34,8 @@ h_k2 = @(c, f_k) c(4)./x(c(1:3), f_k);
 sensor_num = 14;
 a = 160;
 
-%sense_range = sensor_num;
-sense_range = 1:SkinDataSet.sensNum;
+sense_range = sensor_num;
+%sense_range = 1:SkinDataSet.sensNum;
 cont_range = 10;%mm
 cont_res = 100;
 
@@ -49,18 +49,18 @@ for i = sense_range
     c = [s_i0; a];
 
     %Sample many points to form a contour
-%     px = linspace(tpos(1,1)-cont_range, tpos(1,1)+cont_range, cont_res);
-%     py = linspace(tpos(1,2)-cont_range, tpos(1,2)+cont_range, cont_res);
-%     pz = linspace(tpos(1,3)-cont_range, tpos(1,3)+cont_range, cont_res);
-%     [X, Y, Z] = meshgrid(px, py, pz);
-%     V = zeros(size(X));
-%     for l = 1:length(px)
-%         for j = 1:length(py)
-%             for k = 1:length(pz)
-%                 V(l,j,k) = h_k2([px(l), py(j), pz(k), a], tpos(1,:));
-%             end
-%         end
-%     end
+    px = linspace(tpos(1,1)-cont_range, tpos(1,1)+cont_range, cont_res);
+    py = linspace(tpos(1,2)-cont_range, tpos(1,2)+cont_range, cont_res);
+    pz = linspace(tpos(1,3)-cont_range, tpos(1,3)+cont_range, cont_res);
+    [X, Y, Z] = meshgrid(px, py, pz);
+    V = zeros(size(X));
+    for l = 1:length(px)
+        for j = 1:length(py)
+            for k = 1:length(pz)
+                V(l,j,k) = h_k2([px(l), py(j), pz(k), a], tpos(1,:));
+            end
+        end
+    end
 
     %For analyzing the problem:
     %s_i0(1:2) = trueSet.posReal(:,i);
@@ -70,15 +70,15 @@ for i = sense_range
     %s_i_pred = q_new(1:3);
 
     %Plot the data
-%     figure()
-%     hold on; axis equal; grid on;
-%     scatter3(fx(cutoff_indices), fy(cutoff_indices), fz(cutoff_indices), 20, sr(cutoff_indices), 'filled');
-%     scatter3(trueSet.posReal(1,i), trueSet.posReal(2,i), 0, 'g', 'filled');
-%     contourslice(X,Y,Z,V, tpos(1,1), tpos(1,2), tpos(1,3));
-%     scatter3(s_i_pred(1), s_i_pred(2), s_i_pred(3),'b+');
-%     scatter3(path(1,1), path(2,1), path(3,1), 'r+');
-%     plot3(path(1,:), path(2,:), path(3,:), 'k--');
-%     colorbar;
+    figure()
+    hold on; axis equal; grid on;
+    scatter3(fx(cutoff_indices), fy(cutoff_indices), fz(cutoff_indices), 20, sr(cutoff_indices), 'filled');
+    scatter3(trueSet.posReal(1,i), trueSet.posReal(2,i), 0, 'g', 'filled');
+    contourslice(X,Y,Z,V, tpos(1,1), tpos(1,2), tpos(1,3));
+    scatter3(s_i_pred(1), s_i_pred(2), s_i_pred(3),'b+');
+    scatter3(path(1,1), path(2,1), path(3,1), 'r+');
+    plot3(path(1,:), path(2,:), path(3,:), 'k--');
+    colorbar;
 end
 
 
