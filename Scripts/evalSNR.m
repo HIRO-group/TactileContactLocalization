ce4998bbb2626733e34e566d299d91eabeba84d1
Carson Kohlbrenner <82614880+cKohl10@users.noreply.github.com>
2023-12-10 17:22:26 -0700

    added SNR

---
+++ Scripts/evalSNR.m
@@ -0,0 +1,11 @@
+%Finds the SNR of the touchdata
+
+function avgSNR = evalSNR(touchData)
+    touchData.CPL.std = std(touchData.CPL.sensorStateRaw, 0, 1);
+
+    for i = 1:touchData.numSensors
+        touchData.SNR(i) = 20*log10(max(touchData.LIN.sensorStateAvg(i,:))/touchData.CPL.std(i));
+    end
+
+    avgSNR = mean(touchData.SNR);
+end
